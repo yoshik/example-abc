b2ad0e12beb8407d23a64b26346c02746ab81543
yoshik <fujiwara.access@gmail.com>
2024-07-20 00:28:11 +0900

    ボタンのレイアウトの仮置き
    細かい部分が未実装である

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -55,34 +55,44 @@ const Detail = () => {
 	}
 	return (
 		<div className="detail">
-			<div className="book_area">
-				<div
-					className="book"
-					key={book.id_book}
-					style={{ backgroundImage: `url(${book.img_url})` }}
-				></div>
-				<div className="book_description_area">
-					<div className="line name">{book.name_book}</div>
-					<div className="line tagline">
-						<div className="left">
-							<div className="tag">著者</div>
+			<div className="detail_responsive">
+				<div className="book_area">
+					<div
+						className="book"
+						key={book.id_book}
+						style={{ backgroundImage: `url(${book.img_url})` }}
+					></div>
+					<div className="book_description_area">
+						<div className="line name">{book.name_book}</div>
+						<div className="line tagline">
+							<div className="left">
+								<div className="tag">著者</div>
+							</div>
+							<div className="right">{book.author}</div>
 						</div>
-						<div className="right">{book.author}</div>
-					</div>
-					<div className="line tagline">
-						<div className="left">
-							<div className="tag">出版社</div>
+						<div className="line tagline">
+							<div className="left">
+								<div className="tag">出版社</div>
+							</div>
+							<div className="right">{book.publisher}</div>
 						</div>
-						<div className="right">{book.publisher}</div>
-					</div>
-					<div className="line">
-						<button>Mybookに追加</button>
-						<button>読み放題中</button>
+						<div className="line">
+							<button>Mybookに追加</button>
+							<button>読み放題中</button>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="button_area"></div>
+				<div className="button_area">
+					{buttons.map((button) => (
+						<button className="button">
+							<div className="label" key={button.id}>
+								{button.label}
+							</div>
+						</button>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
