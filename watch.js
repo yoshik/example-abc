f2b74417e6f01783b98208374e81888ccd8e326b
yoshik <fujiwara.access@gmail.com>
2024-07-20 00:05:04 +0900

    openapi.yamlの保存時にビルドするようにする

--- watch.js
+++ watch.js
@@ -9,17 +9,18 @@ const log = (err, stdout, stderr) => {
 var prev = "";
 chokidar.watch("./src").on("all", (event, path) => {
 	console.log(event, path);
-	if (path == prev) {
+	if (path === prev) {
 		// フォーマッター自体の保存と連打は無視する
 		setTimeout(() => (prev = ""), 5000);
 	} else {
 		prev = path;
 	}
 	if (event === "change") {
-		if (path.endsWith(".tsx") || path.endsWith(".ts")) {
+		if (path.endsWith("openapi.yaml")) {
+			exec("npm run build:schema", log);
+		} else if (path.endsWith(".tsx") || path.endsWith(".ts")) {
 			exec("npx @biomejs/biome format --write " + path, log);
-		}
-		if (path.endsWith(".css")) {
+		} else if (path.endsWith(".css")) {
 			exec("npx prettier -w " + path, log);
 			exec("npx stylelint " + path + " --fix", log);
 		}
