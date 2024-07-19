daf930d36356b4b6f76adab186c38bf31aeca6eb
yoshik <fujiwara.access@gmail.com>
2024-07-20 00:12:55 +0900

    formatterによるindent修正

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -14,27 +14,25 @@ const List = () => {
 				} else if (state.data == null) {
 					/* loading */
 				} else {
-					return state.data.top_category_list[0].sub_category_list.map(
-						(sub_category) => (
-							<div className="sub_category" key={sub_category.id_category}>
-								<div className="sub_category_name">
-									{sub_category.name_category}
-								</div>
-								<div className="book_list">
-									{(() =>
-										sub_category.book_list.map((book) => (
-											<Link to={"/book/" + book.id_book}>
-												<div
-													className="book"
-													key={book.id_book}
-													style={{ backgroundImage: `url(${book.img_url})` }}
-												></div>
-											</Link>
-										)))()}
-								</div>
+					return state.data.sub_category_list.map((sub_category) => (
+						<div className="sub_category" key={sub_category.id_category}>
+							<div className="sub_category_name">
+								{sub_category.name_category}
 							</div>
-						),
-					);
+							<div className="book_list">
+								{(() =>
+									sub_category.book_list.map((book) => (
+										<Link to={"/book/" + book.id_book}>
+											<div
+												className="book"
+												key={book.id_book}
+												style={{ backgroundImage: `url(${book.img_url})` }}
+											></div>
+										</Link>
+									)))()}
+							</div>
+						</div>
+					));
 				}
 			})()}
 		</div>
